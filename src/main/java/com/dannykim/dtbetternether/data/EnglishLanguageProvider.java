3c7e3c7071b9fc2133df861be45e3d22d4759626
dannykim2011 <danny4257@naver.com>
2026-07-05 00:06:45 +0900

    update

--- src/main/java/com/dannykim/dtbetternether/data/EnglishLanguageProvider.java
+++ src/main/java/com/dannykim/dtbetternether/data/EnglishLanguageProvider.java
@@ -12,7 +12,10 @@ public final class EnglishLanguageProvider extends LanguageProvider {
     @Override
     protected void addTranslations() {
         add("item.dtbetternether.anchor_tree_seed", "Anchor Tree Nut");
+        add("item.dtbetternether.mushroom_fir_seed", "Mushroom Fir Spore");
         add("item.dtbetternether.nether_sakura_seed", "Nether Sakura Seed");
+        add("item.dtbetternether.nether_brown_mushroom_seed", "Nether Brown Mushroom Spore");
+        add("item.dtbetternether.nether_red_mushroom_seed", "Nether Red Mushroom Spore");
         add("item.dtbetternether.rubeus_seed", "Rubeus Cone");
         add("item.dtbetternether.wart_seed", "Wart Spore Gall");
         add("item.dtbetternether.willow_seed", "Nether Willow Inflorescence");
