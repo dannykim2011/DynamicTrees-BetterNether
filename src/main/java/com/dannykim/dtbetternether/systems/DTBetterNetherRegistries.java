bb8c8ced14f993686389bb64faf3c7db6c67620d
dannykim2011 <danny4257@naver.com>
2026-07-17 12:06:41 +0900

    update

--- src/main/java/com/dannykim/dtbetternether/systems/DTBetterNetherRegistries.java
+++ src/main/java/com/dannykim/dtbetternether/systems/DTBetterNetherRegistries.java
@@ -5,14 +5,7 @@ import com.dtteam.dynamictrees.event.RegistryEvent;
 import com.dtteam.dynamictrees.api.worldgen.FeatureCanceller;
 import com.dtteam.dynamictrees.systems.genfeature.GenFeature;
 import com.dtteam.dynamictrees.systems.growthlogic.GrowthLogicKit;
-import com.dtteam.dynamictrees.block.leaves.LeavesProperties;
-import com.dtteam.dynamictrees.block.soil.SoilProperties;
-import com.dtteam.dynamictrees.data.GatherDataHelper;
-import com.dtteam.dynamictrees.tree.family.Family;
-import com.dtteam.dynamictrees.tree.species.Species;
-import com.dtteam.dynamictrees.treepack.Resources;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.data.event.GatherDataEvent;
 public final class DTBetterNetherRegistries {
     private DTBetterNetherRegistries() {}
 
@@ -34,17 +27,4 @@ public final class DTBetterNetherRegistries {
         ModFeatureCancellers.register(event.getRegistry());
     }
 
-    @SubscribeEvent
-    public static void gatherData(final GatherDataEvent event) {
-        Resources.MANAGER.gatherData();
-        if (event instanceof GatherDataEvent.Client clientEvent) {
-            GatherDataHelper.gatherClientData(DynamicTreesBetterNether.MOD_ID, clientEvent,
-                    SoilProperties.REGISTRY, Family.REGISTRY, Species.REGISTRY,
-                    LeavesProperties.REGISTRY);
-        } else if (event instanceof GatherDataEvent.Server serverEvent) {
-            GatherDataHelper.gatherServerData(DynamicTreesBetterNether.MOD_ID, serverEvent,
-                    SoilProperties.REGISTRY, Family.REGISTRY, Species.REGISTRY,
-                    LeavesProperties.REGISTRY);
-        }
-    }
 }
