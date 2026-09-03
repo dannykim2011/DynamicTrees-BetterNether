54ad73a6f3bc98913aba17161b7c51065181a27d
dannykim2011 <danny4257@naver.com>
2026-09-04 00:09:55 +0900

    update

--- src/main/java/com/dannykim/dtbetternether/systems/mushroom/ProtectedMushroomSpecies.java
+++ src/main/java/com/dannykim/dtbetternether/systems/mushroom/ProtectedMushroomSpecies.java
@@ -1,8 +1,6 @@
 package com.dannykim.dtbetternether.systems.mushroom;
 
 import com.ferreusveritas.dynamictrees.api.registry.TypedRegistry;
-import com.ferreusveritas.dynamictrees.entity.FallingTreeEntity;
-import com.ferreusveritas.dynamictrees.entity.animation.AnimationHandler;
 import com.ferreusveritas.dynamictrees.tree.family.Family;
 import com.ferreusveritas.dynamictrees.tree.species.Species;
 import com.ferreusveritas.dynamictreesplus.block.mushroom.CapProperties;
@@ -14,8 +12,5 @@ public final class ProtectedMushroomSpecies extends HugeMushroomSpecies {
             HugeMushroomSpecies.createDefaultMushroomType(ProtectedMushroomSpecies::new);
     public ProtectedMushroomSpecies(final ResourceLocation name, final Family family, final CapProperties capProperties) {
         super(name, family, capProperties);
-    }
-    @Override public AnimationHandler selectAnimationHandler(final FallingTreeEntity entity) {
-        return new MushroomFellingAnimationHandler(super.selectAnimationHandler(entity));
     }
 }
